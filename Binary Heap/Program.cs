0f2085a2a8f3f2876c90c6b2bca9a2505fd45ba5
Jaydeep Shil <jshil@logicx.at>
2020-03-21 13:56:32 +0100

    Add project files.

---
+++ Binary Heap/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Binary_Heap
+{
+    public class BinaryHeap
+    {
+        int capacity;
+        int currectSize;
+
+        public BinaryHeap(int capacity)
+        {
+            this.capacity = capacity;
+        }
+
+        public int rootElement(int position)
+        {
+            return (position - 1) / 2;
+        }
+
+        public int leftChild(int position)
+        {
+            return (position * 2) + 1;
+        }
+
+        protected int rightChild(int position)
+        {
+            return (position * 2) + 2;
+        }
+
+    }
+    class Program : BinaryHeap
+    {
+        const int HEAP_CAPACITY = 10;
+        Program() : base(HEAP_CAPACITY)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            BinaryHeap heap = new BinaryHeap(HEAP_CAPACITY);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
